12e941ac0524725b739f7ce973ced1f0ee115a19
elorm-gershon <alidugershon@gmail.com>
2024-11-07 22:21:15 +0000

    spam fixed

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 const UserProfile = (props) => {
     return (
         <div className='User-Profile' style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-            <h2>
+            
+            <h2 style={{ color: 'blue' }}>
                 {props.name}
             </h2>
 
             <p>
-                Age:{props.age}
+                Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span>
             </p>
 
             <p>Bio:{props.bio}
