5d227eeed6cf66f365211aba9d95e227a131a281
elorm-gershon <alidugershon@gmail.com>
2024-11-17 01:43:59 +0000

    New files added

--- recipe-sharing-app/src/components/favorites.jsx
+++ recipe-sharing-app/src/components/favorites.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { useRecipeStore } from './recipeStore';
 
 const FavoritesList = () => {
-    const favorites = useRecipeStore((state) => state.favorites.map(id =>
+    const favorites = useRecipeStore(state => state.favorites.map(id =>
         state.recipes.find(recipe => recipe.id === id)
     ));
 
@@ -10,14 +9,14 @@ const FavoritesList = () => {
         <div>
             <h2>My Favorites</h2>
             {favorites.length > 0 ? (
-                favorites.map((recipe) => (
+                favorites.map(recipe => (
                     <div key={recipe.id}>
                         <h3>{recipe.title}</h3>
                         <p>{recipe.description}</p>
                     </div>
                 ))
             ) : (
-                <p>No favorites yet. Start adding your favorite recipes!</p>
+                <p>No favorites yet. Add some to see them here!</p>
             )}
         </div>
     );
