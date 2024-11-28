9282d41576655a17ac8cbbf0bff4c68bbf60e301
elorm-gershon <alidugershon@gmail.com>
2024-11-28 23:53:06 +0000

    Tailwind created

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Specify where to look for class usage
+  theme: {
+    extend: {}, // Extend default Tailwind CSS theme here
+  },
+  darkMode: false, // Disable dark mode (optional, can also set to 'media' or 'class')
+  plugins: [], // Add Tailwind CSS plugins here if any
+};
