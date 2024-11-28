62241378cf98485489ec1a01d156d4ffc59ae5f3
elorm-gershon <alidugershon@gmail.com>
2024-11-28 23:56:58 +0000

    files modified

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Specify where to look for class usage
-  theme: {
-    extend: {}, // Extend default Tailwind CSS theme here
-  },
-  darkMode: false, // Disable dark mode (optional, can also set to 'media' or 'class')
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
   plugins: [], // Add Tailwind CSS plugins here if any
 };
